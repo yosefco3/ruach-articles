094a72bb3d2a26449205c5ca42df49a4275931d1
Yosef Cohen <yosefco12@gmail.com>
2026-05-23 15:22:51 +0300

    opus 2

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "./oauth";
+import { setupOAuth } from "./oauth";
 import { uploadRouter } from "../upload";
 import { appRouter } from "../routers";
 import { articleDocxRouter } from "../articleDocxRoute";
@@ -36,7 +36,7 @@ async function startServer() {
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   // OAuth callback under /api/oauth/callback
-  registerOAuthRoutes(app);
+  setupOAuth(app);
   // File upload endpoint
   app.use(uploadRouter);
   // Article Word document download
